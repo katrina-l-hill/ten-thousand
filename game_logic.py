3954e264348f49517fd3aaeb124425c742742f9c
Katrina Hill <khill365@gmail.com>
2022-04-25 14:21:42 -0700

    created GameLogic class and static methods

---
+++ game_logic.py
@@ -0,0 +1,19 @@
+import random
+
+
+class GameLogic:
+    def __init__(self):
+        pass
+
+    # Do this last
+    @staticmethod
+    def calculate_score():
+
+        return
+
+    @staticmethod
+    def roll_dice():
+        roll = random.randint(1, 6)
+        return roll
+
+    print(roll_dice())
