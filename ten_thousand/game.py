b9156416eac6ef9bd9865d8bbd24600efe38771b
Roger Wells <rsw359@gmail.com>
2022-04-27 11:50:11 +0900

    testing testing

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -2,15 +2,26 @@ from ten_thousand.game_logic import GameLogic
 from ten_thousand.banker import Banker
 
 
-
 class Game:
     def __init__(self):
-        pass
+        self.bank = Banker()
+
+    def this_project_succs(self):
+        round_num += 1
+        print(f"Starting round {round_num}")
+        print(f"Rolling {die} dice...")
+        roll_input = ' '.join(map(str, (roller(die))))
+        print(f"*** {roll_input} ***")
+        print("Enter dice to keep, or (q)uit:")
+        response = input("> ")
 
     def play(self, roller=GameLogic.roll_dice):
+
+        def split(word):
+            return [char for char in word]
+
         score = 0
         round_num = 0
-        roll_input = 0
         die = 6
         print('Welcome to Ten Thousand')
         print('(y)es to play or (n)o to decline')
@@ -20,7 +31,7 @@ class Game:
             print("OK. Maybe another time")
         else:
             round_num += 1
-            print("Starting round 1")
+            print(f"Starting round {round_num}")
             print(f"Rolling {die} dice...")
             roll_input = ' '.join(map(str, (roller(die))))
             print(f"*** {roll_input} ***")
@@ -31,20 +42,27 @@ class Game:
                 print("Thanks for playing. You earned 0 points")
 
             else:
-                dice_to_keep = [int(response)]
+                dice_to_keep = [int(x) for x in str(response)]
                 die = die - len(dice_to_keep)
                 score = GameLogic.calculate_score(tuple(dice_to_keep))
-            print(f"You have {score} unbanked points and {die} dice remaining")
-            print("(r)oll again, (b)ank your points or (q)uit:)")
-            response = input("> ")
-            if response == 'b':
-                Banker.shelf(score)
+                self.bank.shelf(score)
                 print(f"You have {score} unbanked points and {die} dice remaining")
+                print("(r)oll again, (b)ank your points or (q)uit:)")
+                response = input('> ')
 
+                if response == 'b':
+                    score = self.bank.bank()
+                    print(f"You have {score} banked points in round {round_num}")
+                    print(f"Total score is {score} points")
+                    round_num += 1
+                    print(f"Starting round {round_num}")
+                    roll_input = ' '.join(map(str, (roller(die))))
+                    print(f"*** {roll_input} ***")
+                    print("Enter dice to keep, or (q)uit:")
+                    response = input("> ")
 
-
-
-
+                    if response == "q":
+                        print(f"Thanks for playing. You earned {score} points")
 
             # # turn the string 3335 into a list [3,3,3,5]
             # else:
