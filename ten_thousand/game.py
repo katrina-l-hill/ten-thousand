099d296fab7f16d4bbe41a9bbd62955a29311319
Katrina Hill <khill365@gmail.com>
2022-04-29 23:03:07 -0700

    removed comments, modified variable names

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -22,7 +22,6 @@ class Game:
             return False
 
     def rounds(self, total, local_total, round_num, die, roller):
-        round_num += 1
         input_is_valid = True  # flag
         zilch = False
 
@@ -32,7 +31,9 @@ class Game:
 
         while input_is_valid is True:
             roll = list(roller(die))
-            roll_input = " ".join(map(str, roll))  # removing everything except for the numbers, using them on line 27
+            roll_input = " ".join(
+                map(str, roll)
+            )  # removing everything except for the numbers, using them on line 27
             print(f"*** {roll_input} ***")
             zilch = self.check_for_zilch(roll)
             if zilch:
@@ -54,45 +55,58 @@ class Game:
                         response = input("> ")
 
                         if response == "q":
-                            print(f"Thanks for playing. You earned {self.bank.balance} points")
+                            print(
+                                f"Thanks for playing. You earned {self.bank.balance} points"
+                            )
                             sys.exit()
                         else:
                             die = die - len(dice_to_keep)
-                            local_total += GameLogic.calculate_score(tuple(dice_to_keep))
-                            print(f"You have {local_total} unbanked points and {die} dice remaining")
+                            local_total += GameLogic.calculate_score(
+                                tuple(dice_to_keep)
+                            )
+                            print(
+                                f"You have {local_total} unbanked points and {die} dice remaining"
+                            )
                             print("(r)oll again, (b)ank your points or (q)uit:")
                             response = input("> ")
 
                             if response == "r":
                                 roll = list(roller(die))
                                 roll_input = " ".join(
-                                    map(str, roll))  # removing everything except for the numbers, using them on line 27
+                                    map(str, roll)
+                                )  # removing everything except for the numbers, using them on line 27
                                 print(f"*** {roll_input} ***")
                                 zilch = self.check_for_zilch(roll)
                                 if zilch:
                                     self.zilch_is_yes(round_num, self.bank.balance)
                                 else:
                                     dice_to_keep = [int(x) for x in str(response)]
-                                    input_is_valid = GameLogic.validate_keepers(roll, dice_to_keep)
+                                    input_is_valid = GameLogic.validate_keepers(
+                                        roll, dice_to_keep
+                                    )
                                     if input_is_valid is False:
                                         print(f"*** {roll_input} ***")
                                         print("Enter dice to keep, or (q)uit:")
                                         response = input("> ")
-                                        if die = 0:
-                                        ## need to say game over if dice is 0
-
+                                        if die == 0:
+                                            ## need to say game over if dice is 0
+                                            pass
 
                             elif response == "b":
                                 self.bank.shelf(local_total)
                                 local_total = self.bank.bank()
-                                print(f"You banked {local_total} points in round {round_num}")
+                                print(
+                                    f"You banked {local_total} points in round {round_num}"
+                                )
                                 print(f"Total score is {self.bank.balance} points")
                                 local_total = 0
                                 die = 6
                                 self.play()
 
                             elif response == "q":
-                                print(f"Thanks for playing. You earned {self.bank.balance} points")
+                                print(
+                                    f"Thanks for playing. You earned {self.bank.balance} points"
+                                )
                                 sys.exit()
 
     def play(self, roller=GameLogic.roll_dice):
@@ -110,7 +124,7 @@ class Game:
             print("OK. Maybe another time")
 
         if response == "y":
-            while True:
+            while round_num < 21:
                 round_num += 1
                 self.rounds(total, local_total, round_num, die, roller)
 
@@ -130,6 +144,8 @@ class Game:
 
     @staticmethod
     def game_over():
+        pass
+
 
 if __name__ == "__main__":
     game = Game()
